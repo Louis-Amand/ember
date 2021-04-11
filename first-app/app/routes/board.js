d183233572cbdfbc1e65063b0aae0a6f696ef9af
Louis-Amand <21807526@etu.unicaen.fr>
2021-04-11 19:41:49 +0200

    finition du bouton de deco par ajout action

--- first-app/app/routes/board.js
+++ first-app/app/routes/board.js
@@ -1,17 +1,27 @@
-import Abstractroute from './abstractroute';
+import AbstractrouteRoute from './abstractroute';
 import RSVP from 'rsvp';
+import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
-export default class BoardRoute extends Abstractroute {
+export default class BoardRoute extends AbstractrouteRoute {
+  @service userAuth; // ajout du service pour la deconnexion
+
   model() {
-  let user = this.userAuth.user;
-  if (user) {
-    return RSVP.hash({
-      orders: this.store.query('order', {
-        filter: { idEmployee: user.id },
-        include: 'orderdetails',
-      }),
-      employee: user,
-    });
+    let user = this.userAuth.user;
+    if (user) {
+      var retour = RSVP.hash({
+        orders: this.store.query('order', {
+          filter: { idEmployee: user.id },
+          include: 'orderdetails',
+        }),
+        employee: user,
+      });
+      return retour;
+    }
   }
-}
+
+  @action logout() { // action DECONEXION 
+    this.userAuth.logout();
+    this.transitionTo('index');
+  }
 }
