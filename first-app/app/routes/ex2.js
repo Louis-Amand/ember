45f626a30245efbbbf9a2f744a25f322ce2a97a2
Louis-Amand <21807526@etu.unicaen.fr>
2021-04-24 13:02:36 +0200

    ajout du toggleAction import reussi

--- first-app/app/routes/ex2.js
+++ first-app/app/routes/ex2.js
@@ -1,9 +1,14 @@
 import Route from '@ember/routing/route';
 import { datas } from 'first-app/data/datas';
 import Services from 'first-app/classes/services';
- 
+import {action, set} from '@ember/object';
+
 export default class Ex2Route extends Route {
   model() {
     return new Services(datas);
+  }
+
+  @action toggleActive(s) {
+    set(s, 'active', !s.active);
   }
 }
