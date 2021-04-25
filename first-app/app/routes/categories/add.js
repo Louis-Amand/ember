a29708136fe870e62111442317472e5b428cd299
Louis-Amand <21807526@etu.unicaen.fr>
2021-04-25 14:11:53 +0200

    modification de la route add

--- first-app/app/routes/categories/add.js
+++ first-app/app/routes/categories/add.js
@@ -1,4 +1,13 @@
-import Route from '@ember/routing/route';
+import Route from '@ember/routing/route'; 
+import { action } from '@ember/object'; 
 
 export default class CategoriesAddRoute extends Route {
+  model() {
+    return {}; // retourne un objet JS vide
+  }
+
+  @action save(data) {  //Sauvegarde la catégorie @action
+    let category = this.store.createRecord('category', data);
+    category.save();
+  }
 }
